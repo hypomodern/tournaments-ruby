92c5a3f05ce0a601d734d68aacd12c915c62bb78
Matt Wilson <mhw@hypomodern.com>
2014-07-25 07:34:47 -0400

    Test harness setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'tournaments'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+end
---
+++ spec/tournaments_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Tournaments do
+  it "is setup correctly" do
+    expect(Tournaments::VERSION).to eq('0.0.1')
+  end
+end
